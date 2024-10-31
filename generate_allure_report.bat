fbb85d4a1201bffd0784437cdea684aeb6cce656
mosta <mostafa_mohab18@hotmail.com>
2024-10-31 13:20:40 +0300

    Add bat file Updates

--- generate_allure_report.bat
+++ generate_allure_report.bat
@@ -1,6 +1,6 @@
 @echo off
 :: If you already have a valid JAVA_HOME environment variable set, feel free to comment the below two lines
-set JAVA_HOME=C:\Users\mosta\.jdks\corretto-21.0.5
+set JAVA_HOME=C:\Program Files\Java\jdk-21
 set path=%JAVA_HOME%\bin;%path%
 set path=C:\Users\mosta\.m2\repository\allure\allure-2.25.0\bin;%path%
 allure serve allure-results -h localhost
